00dc7fe1f41e839770c53b1ddf76c2e3af8f3a33
Nicholas Hoban <nickslaptop@Nicholass-MacBook-Pro.local>
2021-04-28 13:42:16 -0400

    still buggy

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -68,12 +68,6 @@ const tableOfContentsContributing = contributing => {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-const tableOfContentsLicense = license => {
-  if (!license) {
-    return '';
-  }
-  return `# [License](#license)`
-}
 
 // TODO: Create a function to generate markdown for README
 module.exports = generateMarkdown => {
